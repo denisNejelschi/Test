5a7770c73ca9db83a22f7690847fd0cf7b70bd29
Denis Nejelschi <nejelschi@gmail.com>
2023-11-01 20:15:05 +0100

    fixing error

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 // метод который будет обЬединять массив цифр в одно цисло  
-public class git Hello {
+public class  Hello {
 
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
