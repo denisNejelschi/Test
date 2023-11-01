d713e734ad20145f15ce5d6b90d0e567052ffa34
Denis Nejelschi <nejelschi@gmail.com>
2023-11-01 20:13:03 +0100

    change swich

--- src/Main01.java
+++ src/Main01.java
@@ -7,7 +7,7 @@ public class Main01 {
   // 42011 % 100
 
   public static String inclineHoursWord(int hours) {
-    // если 11-14, надо выводить "часов" в любом случае
+
     switch (hours % 100) {
       case 11:
       case 12:
@@ -20,7 +20,7 @@ public class Main01 {
     switch (hours % 10) {
       case 1:
         return "час";
-      // делаем брейкаут чтобы для трех значений сразу делать один вывод
+
       case 2:
       case 3:
       case 4:
@@ -29,8 +29,7 @@ public class Main01 {
       case 6:
       case 7:
       case 8:
-      case 9:
-      case 0:
+
       default:
         return "часов";
     }
@@ -40,7 +39,7 @@ public class Main01 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int hours = scanner.nextInt();
-    System.out.println("коммент для обучения ");
+    System.out.println("коммент для обучения в гитхабе ");
     System.out.println(hours + " " + inclineHoursWord(hours));
   }
 }
